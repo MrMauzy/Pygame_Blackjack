9e1918b5d2bd7092eb856dd629a9db025260d2ba
Matt Mauzy <mrmauzy13@gmail.com>
2021-09-10 13:14:14 -0500

    Started adding scoring system

--- main.py
+++ main.py
@@ -46,6 +46,12 @@ club_K = pygame.transform.scale(club_KO, (CARD_WIDTH, CARD_HEIGHT))
 
 #Global Constants
 GREEN = (34,139,34)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+PURPLE = (127,0,255)
+clock = pygame.time.Clock()
+
+gameoverText = font.render("Restart? ", True, PURPLE)
 
 cards = { club_A:1, club_2:2, club_3:3, club_4:4, club_5:5, club_6:6, club_7:7, 
         club_8:8, club_9:9, club_J:10, club_Q:11, club_K:12 }
@@ -55,11 +61,11 @@ def draw_window():
     pygame.display.update()
 
 def getValue(card):
-    return 
+    return cards.get(card)
     
 def main():
 
-    userScore = cards.get(club_A)
+    userScore = 0
     dealerScore = 0
 
     userCard = []
@@ -72,32 +78,42 @@ def main():
     dealCard.append(random.choice(list(cards)))
 
     
-    userScoreText = font.render("Player: " + str(userScore), False, (255,255,255))
-    dealScoreText = font.render("House: " + str(dealerScore), False, (255,255,255))
+
 
     # Main loop
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(5)
+    while True:
+        gameover = True  if (userScore >= 21) else False
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and gameover and restartB.collidepoint(pygame.mouse.get_pos()):
+                gameover = True
 
         draw_window()
-
+        userScoreText = font.render("Player: " + str(userScore), False, (255,255,255))
+        dealScoreText = font.render("House: " + str(dealerScore), False, (255,255,255))
         WIN.blit(userScoreText, (10,400))
         WIN.blit(dealScoreText, (10,100))
 
+
+
         for card in dealCard:
+            #dealerScore = 10 #+= getValue(card)
             WIN.blit(card, (350, 100))
         WIN.blit(cBack, (410,100))
 
         for card in userCard:
+            #userScore += getValue(card)
             x = 350 + (userCard.index(card)*60)
             WIN.blit(card, (x, 400))
+        
+        if gameover:
+            WIN.blit(gameoverText, (250,250))
+            restartB = pygame.draw.rect(WIN, WHITE, (WIDTH/2, HEIGHT/2, 140, 40))
 
         pygame.display.update()
+        clock.tick(10)
 
 
         
